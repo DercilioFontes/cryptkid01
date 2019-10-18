742ee9bb4d86c32b7e0ec74a05cb552d24f10643
DercilioFontes <derciliofontes@gmail.com>
2019-10-18 13:47:30 -0700

    general changes

--- force-app/main/default/lwc/cryptoKidComponent/cryptoKidComponent.js
+++ force-app/main/default/lwc/cryptoKidComponent/cryptoKidComponent.js
@@ -1,7 +1,22 @@
 import { LightningElement, track } from "lwc";
+import apex from '@salesforce/apex';
+
 
 export default class ComboboxBasic extends LightningElement {
   @track value = 0;
+  @track valueFormated = 0;
+  data = [];
+
+  public ComboboxBasic() {
+    // axios.get("api.coincap.io/v2/assets?limit=10")
+    //   .then(response => {
+    //     // eslint-disable-next-line no-console
+    //     console.log(response.data);
+    //     this.data = response.data;
+    //   })
+
+    apex.get()
+  }
 
   get options() {
     return [
@@ -15,5 +30,9 @@ export default class ComboboxBasic extends LightningElement {
 
   handleChange(event) {
     this.value = event.detail.value;
+    this.valueFormated = this.value.toLocaleString("en-US", {
+      style: "decimal",
+      maximumFractionDigits: 2
+    });
   }
 }
